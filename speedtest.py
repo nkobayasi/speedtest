65435d165b0095ca7611afe4dea5f0e888c87874
nkobayasi <noriyuki.kobayasi@gmail.com>
2024-02-01 13:15:18 +0900

    プロパティ経由に変更

--- speedtest.py
+++ speedtest.py
@@ -461,7 +461,7 @@ class Server(object):
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0',
                     'Cache-Control': 'no-cache',
                     'Content-Type': 'application/x-www-form-urlencoded',
-                    'Content-Length': size, },
+                    'Content-Length': data.size, },
                 data=data)
             logger.debug(request.full_url)
             requestq.put(request)
