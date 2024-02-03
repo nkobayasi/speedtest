2be5762caabc949d1ef5ac7564c4a93c9826a7ee
nkobayasi <noriyuki.kobayasi@gmail.com>
2024-02-03 18:20:51 +0900

    serversプロパティのIterator化

--- speedtest.py
+++ speedtest.py
@@ -720,6 +720,17 @@ class Servers(object):
                     continue
                 servers.append(server)
         return servers
+    
+    def __iter__(self):
+        self._iter_index = 0
+        return self
+    
+    def __next__(self):
+        if self._iter_index < len(self.servers):
+            result = self.servers[self._iter_index]
+            self._iter_index += 1
+            return result
+        raise StopIteration()
                 
     def get_closest_servers(self, limit=5):
         def sort_by_distance(servers):
